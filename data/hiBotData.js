ce89424d86eedf814cfe04d55e5a56ddb0fff083
Diogo Reis <diogommreis1@gmail.com>
2020-06-28 01:35:25 +0100

    New phrases to the bot

---
+++ data/hiBotData.js
@@ -0,0 +1,14 @@
+var data = {
+  first: "Oi, mô",
+  second: "Oii, bebe",
+  third: "Ei, delicia",
+  fourth: "Fala com o pai",
+  fifth: "Manda a braba mnw",
+  sixth: "Olá, Vossa excelência",
+  seventh: "Pode falar!",
+  eighth: "Cheguei!",
+  ninth: "Fala a boa ae",
+  tenth: "Então pa, tás bem?"
+}
+
+module.exports = data;
